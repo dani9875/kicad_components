580dd05bcdf1a1e8283c2a30f72ecf4e37409c28
dani9875 <dan.hatar94@gmail.com>
2024-07-16 11:54:03 +0200

    Ignoring test point and mounting hole libraries

--- tools/unifier.sh
+++ tools/unifier.sh
@@ -8,6 +8,11 @@ for SUBDIR in "$ROOT_DIR"/*; do
   if [ -d "$SUBDIR" ]; then
     SUBDIR_NAME=$(basename "$SUBDIR")
 
+    # Exclude specified folders
+    if [[ "$SUBDIR_NAME" == "[MOUNTING_HOLES]" || "$SUBDIR_NAME" == "[TEST_POINTS]" ]]; then
+      continue
+    fi
+
     # Loop through each file in the subdirectory
     for FILE in "$SUBDIR"/*; do
       if [ -f "$FILE" ]; then
@@ -21,5 +26,4 @@ for SUBDIR in "$ROOT_DIR"/*; do
   fi
 done
 
-echo "Files have been renamed."
-
+echo "Files have been renamed, excluding MOUNTING_HOLES and TEST_POINTS directories."
